f0ee43d88b440d9c70107c75d0f84b941d1ee934
Chris Moore <csmooreacm@acm.org>
2015-12-09 13:57:27 -0500

    Add Apache License Headers

--- scripts/NAMDownload.py
+++ scripts/NAMDownload.py
@@ -1,18 +1,31 @@
-##Name: Download NOAA data
-##
-##Description: Downloads the most up to date data from the NOAA site by getting the present date.
-##
-##                Script works as follow;
-##                    Gets the present time date in UTC
-##                    Uses the OPeNDAP to NetCDF tool from Multidimension Supplimental Tool
-##                    Downloads the specified variables into NetCDF format files and saves them in the relative location, based on where the script file is located.
-##                    The present data is removed from the Mosaic Dataset
-##                    The new data is then loaded into the Mosiac Dataset
-##
-##Date Edited: 21/09/2015
+# ----------------------------------------------------------------------------------
+# Copyright 2015 Esri
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+# Name: Download NOAA data
+#
+# Description: Downloads the most up to date data from the NOAA site by getting the present date.
+#
+# Script works as follow;
+#      Gets the present time date in UTC
+#      Uses the OPeNDAP to NetCDF tool from Multidimension Supplimental Tool
+#      Downloads the specified variables into NetCDF format files and saves them in the relative location, based on where the script file is located.
+#      The present data is removed from the Mosaic Dataset
+#      The new data is then loaded into the Mosiac Dataset
+#
+# Date Edited: 21/09/2015
+# ----------------------------------------------------------------------------------
 
-
-#Import modules
 import arceditor
 import arcpy
 import os
--- scripts/WindDirectionFromUV.py
+++ scripts/WindDirectionFromUV.py
@@ -1,12 +1,23 @@
+# ----------------------------------------------------------------------------------
+# Copyright 2015 Esri
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
 # Name: WindDirectionFromUV
-
 # Description: Python raster function to extract wind direction from u and v components of wind.  It is assumed that the
 # wind direction being fed into this function is the normal meterological convention of the direction the wind is coming
 # from and not the direction it is blowing.
-
 # Date Edited: 22/09/2015
-
-#----------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
 
 import numpy as np
 
